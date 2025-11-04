384a3c76314d41abe25de1e5ec94d6592270d80e
Rahul Rai <rahulroy.global@gmail.com>
2025-11-04 16:33:50 +0530

    Chnage in App.js API_BASE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://backend-app:5000';
+const API_BASE = process.env.REACT_APP_API_URL || '/api';
 
 
 function App() {
